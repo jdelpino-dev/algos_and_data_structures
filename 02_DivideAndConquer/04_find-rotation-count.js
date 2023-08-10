dedd591d506fbbcc37a2fcdcc0a594ace503e30d
José Delpino <delpinoivivas@gmail.com>
2023-08-10 15:20:04 -0500

    Implemented find rotation count

--- 02_DivideAndConquer/04_find-rotation-count.js
+++ 02_DivideAndConquer/04_find-rotation-count.js
@@ -5,35 +5,45 @@
   Solution by José Delpino
 */
 
-/** Recursively counts the number of rotations of a sorted array of integers
- * that was rotated an unknown number of times. Uses a divide and conquer
- * approach.
+/** Counts the number of rotations of a sorted array of integers
  *
  * @param {number[]} array // sorted & rotated array of integers
  * @return {number} // number of rotations
  *
+ * Given a rotated array of sorted integers, returns the number of rotations.
+ * First finds the rotation pivot using a helper function –findRotationPivot()–
+ * that uses a divide and conquer approach.
  */
-function findRotationCount(array, start = 0, end = array.length - 1) {
+function findRotationCount(array) {
+  let rotationPivot = findRotationPivot(array);
+  return rotationPivot === -1 ? 0 : rotationPivot + 1;
+}
+
+/** Recursively searches for the rotation pivot of a rotated array of sorted
+ * integers. Uses a divide and conquer approach.
+ * @param {number[]} array // sorted & rotated array of integers
+ * @param {number} start // start index of the search in array
+ * @param {number} end // end index of the search in array
+ * @return {number} // index of the rotation pivot or -1 if not found.
+ *                     rotation pivot  = the last element of the
+ *                                       left partition of the array.
+ */
+function findRotationPivot(array, start = 0, end = array.length - 1) {
   // Calculates the length of the search partition.
   let partitionLength = end - start + 1;
 
   // Base cases:
 
-  // 1. If the partition has one element left or none, then there are no
-  //    rotations.
+  // 1. If the partition has one element left or none, then return -1.
   if (partitionLength === 0 || partitionLength === 1) {
-    return 0;
-    // 2. If the partition has only two elements, checks if there is a rotation
-    //    and returns the result.
+    return -1;
+    // 2. If the partition has only two elements, either the first is the
+    //    pivot or there is no pivot.
   } else if (partitionLength === 2) {
     return array[start] > array[start + 1] ? start : -1;
   }
 
   // Preparing the recursive call:
-
-  // Initialize the counter of rotations.
-  let rotationCounter = 0;
-
   // Calculates the middle index of the array, gets the middle element, and
   // its left and right neighbors.
   let middle = Math.floor(start + (end - start) / 2);
@@ -43,16 +53,26 @@ function findRotationCount(array, start = 0, end = array.length - 1) {
   let rightNeighbor = array[middle + 1]; // undefined if middle is
   //                                        the last element of the array
 
-  // If we found a rotation, we add it to the counter. We found a rotation
-  // if the middle element is bigger than both the element before
-  //    and after it. If any of the neighbors is undefined,
-  //    that won't be a problem: JS will return false for the comparison.
+  // There are 2 possible options in order to find the pivot in a partition
+  // with 3 ore more elements:
+  // 1. The middle element is the pivot. That's the case when it is bigger
+  //    than both the element before and after it. If any of the neighbors
+  //    is undefined, that won't be a problem: JS will return false
+  //    for the comparison.
   if (middleElem > leftNeighbor && middleElem > rightNeighbor) {
-    rotationCounter++; // We add 1 to the counter because we found a rotation.
+    return middle; // We got the pivot!
   }
-
-  // We need to do double recursive call to calculate the number of rotations
-  // in the left and right sides of the array.
-  let leftSide = findRotationPivot(array, start, middle);
-  let rightSide = findRotationPivot(array, middle + 1, end);
+  // 2. A double recursive case is needed because the middle element
+  //    is not the pivot and then we don't know where is it. We need to
+  //    look for it in both the left and right sides. We must do 2 recursive
+  //    calls, one for each side.
+  else {
+    // We must include the middle element in the left
+    // call because if not we could miss vital information to find the pivot.
+    let leftSide = findRotationPivot(array, start, middle);
+    let rightSide = findRotationPivot(array, middle + 1, end);
+    // We return the result of the recursive calls. If the pivot is not
+    // found, then -1 will be returned.
+    return leftSide !== -1 ? leftSide : rightSide;
+  }
 }
