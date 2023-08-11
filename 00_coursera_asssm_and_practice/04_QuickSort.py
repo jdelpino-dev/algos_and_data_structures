7cdcb043cea34d8eff10a73fd0033e27afcbd9c2
José Delpino <delpinoivivas@gmail.com>
2023-08-11 18:46:10 -0500

    Fixed all the bugs of Quicksort!

--- 00_coursera_asssm_and_practice/04_QuickSort.py
+++ 00_coursera_asssm_and_practice/04_QuickSort.py
@@ -21,13 +21,84 @@ def quick_sort(
         * If count_comparisons is False, return sorted array
         * If count_comparisons is True, return a tuple of
         sorted array and number of comparisons
+
+    DocTests:
+
+        >>> array = [1, 2, 3]
+        >>> quick_sort(
+        ...     array, 0, len(array) - 1, pivot_random,
+        ...     count_comparisons=False
+        ... )
+        >>> array == [1, 2, 3]
+        True
+
+        >>> array =[6, 1, 2, 7]
+        >>> quick_sort(
+        ...     array, 0, len(array) - 1, pivot_random,
+        ...     count_comparisons=False
+        ... )
+        >>> array == [1, 2, 6, 7]
+        True
+
+        >>> array = [5, 4, 3, 2, 1]
+        >>> quick_sort(
+        ...     array, 0, len(array) - 1, pivot_random,
+        ...     count_comparisons=False
+        ... )
+        >>> array == [1, 2, 3, 4, 5]
+        True
+
+        >>> array = []
+        >>> quick_sort(
+        ...     array, 0, len(array) - 1, pivot_random,
+        ...     count_comparisons=False
+        ... )
+        >>> array == []
+        True
+
+        >>> array = [45]
+        >>> quick_sort(
+        ...     array, 0, len(array) - 1, pivot_random,
+        ...     count_comparisons=False
+        ... )
+        >>> array == [45]
+        True
+
+        >>> array = [78, 8]
+        >>> quick_sort(
+        ...     array, 0, len(array) - 1, pivot_random,
+        ...     count_comparisons=False
+        ... )
+        >>> array == [8, 78]
+        True
+
+        >>> array = [28, 8, 44, 17, 9, 31, 60, 101, 82, 2]
+        >>> quick_sort(
+        ...     array, 0, len(array) - 1, pivot_random,
+        ...     count_comparisons=False
+        ... )
+        >>> array == [2, 8, 9, 17, 28, 31, 44, 60, 82, 101]
+        True
+
     """
+    # Base case
+    if start >= end:
+        return
+
     pivot = choose_pivot(array, start, end)
     # Moves the pivot to the beggining of the partition
-    swap(start, pivot)
+    swap(array, start, pivot)
+
+    # Partition the array and gets new pivot index
+    pivot = partition(array, start, end)
+
+    quick_sort(array, start, pivot - 1, choose_pivot,
+               count_comparisons=count_comparisons)
+    quick_sort(array, pivot + 1, end, choose_pivot,
+               count_comparisons=count_comparisons)
 
 
-def partition(array: list, start: int, end: int) -> None:
+def partition(array: list, start: int, end: int) -> int:
     """
     This function partitions an array around the first element.
 
@@ -45,12 +116,16 @@ def partition(array: list, start: int, end: int) -> None:
     partition_boundary = pivot + 1
 
     for current in range(start + 1, end + 1):
-        if array[next] < pivot:  # else do nothing
-            swap(partition_boundary, current)
+        if array[current] < array[pivot]:  # else do nothing
+            swap(array, partition_boundary, current)
             # Restores partition invariant arounf pivot
-            partition_boundary = + 1
-        # Places the pivot in the right place with a swap.
-        swap(pivot, partition_boundary - 1)
+            partition_boundary += 1
+    # Places the pivot in the right place with a swap.
+    swap(array, pivot, partition_boundary - 1)
+    # Updates its index
+    pivot = partition_boundary - 1
+
+    return pivot
 
 
 def swap(array: list, element1: int, element2: int):
@@ -69,7 +144,7 @@ def pivot_first(array: list, start: int, end: int) -> int:
     """
     This function partitions an array around the first element.
     """
-    return 0
+    return start
 
 
 def pivot_last(array: list, start: int, end: int) -> int:
@@ -77,7 +152,7 @@ def pivot_last(array: list, start: int, end: int) -> int:
     This function partitions an array around the last element.
 
     """
-    return len(array)
+    return end
 
 
 def pivot_median_of_three(array: list, start: int, end: int) -> int:
@@ -86,19 +161,25 @@ def pivot_median_of_three(array: list, start: int, end: int) -> int:
     the first, middle and last element.
     """
     first = array[start]
-    last = array[len(array)]
+    last = array[end]
 
-    middle_index = (end - start // 2) + start
+    middle_index = start + (end - start) // 2
     middle = array[middle_index]
+    median_element = median(first, middle, last)
 
-    return median(first, middle, last)
+    if median_element == first:
+        return start
+    if median_element == middle:
+        return middle
+    if median_element == last:
+        return end
 
 
 def pivot_random(array: list, start: int, end: int) -> int:
     """
     This function partitions an array around a random element.
     """
-    return randint(0, len(array))
+    return randint(start, end)
 
 
 def array_from_file(filename):
