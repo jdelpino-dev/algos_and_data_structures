345e7d0cdf1a18596b4c3a823e9f77c9a8fcc711
José Delpino <delpinoivivas@gmail.com>
2023-08-16 11:27:38 -0500

    Implemented Karger

---
+++ 00_coursera_asssm_and_practice/06_KargerMinCut.py
@@ -0,0 +1,64 @@
+from graph1 import Graph
+from random import randint
+
+
+def repeat_karger_min_cuts(original_graph: Graph, times: int) -> int:
+    """
+    Return the minimum number of cuts in a graph.
+    """
+    min_cuts = float("inf")
+
+    for _ in range(times):
+        seed = randint(1, 3_0043_110)
+        contraction_graph = Graph.copy(original_graph)
+        contraction_graph.turn_into_multigraph()
+        cuts = karger_min_cuts(contraction_graph, seed)
+        if cuts < min_cuts:
+            min_cuts = cuts
+        # print(f"Current min cuts: {min_cuts}")
+
+    return min_cuts
+
+
+def karger_min_cuts(contraction_graph: Graph, seed_value) -> int:
+    """
+    Return the minimum number of cuts in a graph.
+    """
+
+    # Contract the graph until there are only two nodes left.
+    while (len(contraction_graph.nodes) > 2
+           and len(contraction_graph.edges) > 0):
+        edge = contraction_graph.random_edge(seed_value=seed_value)
+        contraction_graph.contract_nodes(*edge)
+        contraction_graph.delete_loops(edge[0])
+
+    # Get one of the two remaining nodes.
+    graphnodes = list(contraction_graph.nodes)
+    node = graphnodes[0]
+    # Return the number of edges between the two remaining nodes.
+    return contraction_graph.degree(node)
+
+
+# Main block
+if __name__ == "__main__":
+    # filename = (
+    #     "./20_algos_and_data_structures/00_coursera_asssm_and_practice"
+    #     "/05_data/graph_1.txt"
+    # )
+    # filename = ("./00_coursera_asssm_and_practice"
+    #             "/05_data/graph_1.txt")
+    # filename = ("00_coursera_asssm_and_practice"
+    #             "/05_data/graph_1.txt")
+    # filename = "05_data/graph_1.txt"
+    # filename = "./05_data/graph_1.txt"
+    filename = ("/Users/jdelpino/Library/Mobile Documents/"
+                "com~apple~CloudDocs/Docus-Nube/SWE & AI/code_workspaces/"
+                "swe-proyects/springboard_code/20_algos_and_data_structures/"
+                "00_coursera_asssm_and_practice/05_data/graph_1.txt")
+
+    graph = Graph.from_file(filename,
+                            node_type=int,
+                            directed=False,
+                            multigraph=False)
+    # print(repeat_karger_min_cuts(graph, 306000))
+    print(repeat_karger_min_cuts(graph, 50))
