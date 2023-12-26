6db4b95613e369b3abb6b80a9caf25f03119a615
José Delpino <delpinoivivas@gmail.com>
2023-12-26 14:13:23 -0600

    Added JSDocs to starter file and implemented the push method

--- 03_linked_list/linked-list.js
+++ 03_linked_list/linked-list.js
@@ -1,6 +1,14 @@
 /** Node: node for a singly linked list. */
 
+/** Node: constructor for node class.
+ * @property {any} val
+ * @property {Node} next
+ */
 class Node {
+  /**
+   * Constructs a new node with the given value.
+   * @param {any} val
+   */
   constructor(val) {
     this.val = val;
     this.next = null;
@@ -9,68 +17,69 @@ class Node {
 
 /** LinkedList: chained together nodes. */
 
+/** LinkedList: chained together nodes.
+ * @property {Node} head
+ * @property {Node} tail
+ * @property {number} length
+ */
 class LinkedList {
+  /**
+   * Constructs a new linked list instance.
+   * @param {any[]} vals
+   */
   constructor(vals = []) {
     this.head = null;
     this.tail = null;
     this.length = 0;
 
-    for (let val of vals) this.push(val);
+    for (const val of vals) this.push(val);
   }
 
-  /** push(val): add new value to end of list. */
-
+  /** Add new value to end of list.
+   * @param {any} val
+   */
   push(val) {
-
+    const newNode = new Node(val);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }
   }
 
   /** unshift(val): add new value to start of list. */
 
-  unshift(val) {
-
-  }
+  unshift(val) {}
 
   /** pop(): return & remove last item. */
 
-  pop() {
-
-  }
+  pop() {}
 
   /** shift(): return & remove first item. */
 
-  shift() {
-
-  }
+  shift() {}
 
   /** getAt(idx): get val at idx. */
 
-  getAt(idx) {
-
-  }
+  getAt(idx) {}
 
   /** setAt(idx, val): set val at idx to val */
 
-  setAt(idx, val) {
-
-  }
+  setAt(idx, val) {}
 
   /** insertAt(idx, val): add node w/val before idx. */
 
-  insertAt(idx, val) {
-
-  }
+  insertAt(idx, val) {}
 
   /** removeAt(idx): return & remove item at idx, */
 
-  removeAt(idx) {
-
-  }
+  removeAt(idx) {}
 
   /** average(): return an average of all values in the list */
 
-  average() {
-    
-  }
+  average() {}
 }
 
-module.exports = LinkedList;
+export default LinkedList;
